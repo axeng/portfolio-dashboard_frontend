a92c6776f20a2113643a6de1ae5a9f3e4fbfe719
Axen Georget <mail@axen.io>
2021-03-19 00:01:15 +0100

    refactor: remove prettier and fix coding style + add keycloak-js package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
-  root: true,
+    root: true,
 
-  env: {
-    node: true
-  },
+    env: {
+        node: true
+    },
 
-  parserOptions: {
-    parser: "@typescript-eslint/parser"
-  },
+    parserOptions: {
+        parser: "@typescript-eslint/parser"
+    },
 
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    },
 
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/prettier",
-    "@vue/typescript"
-  ]
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/typescript"
+    ]
 };
--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM node:14.15-alpine3.13
 
 ARG VUE_APP_BACKEND_ADDRESS
+ARG VUE_APP_KEYCLOAK_ADDRESS
+ARG VUE_APP_KEYCLOAK_REALM
+ARG VUE_APP_KEYCLOAK_CLIENT_ID
 
 WORKDIR /frontend
 
@@ -11,6 +14,9 @@ RUN yarn install
 COPY . /frontend/
 
 RUN echo "VUE_APP_BACKEND_ADDRESS=${VUE_APP_BACKEND_ADDRESS}" >> .env.production.local
+RUN echo "VUE_APP_KEYCLOAK_ADDRESS=${VUE_APP_KEYCLOAK_ADDRESS}" >> .env.production.local
+RUN echo "VUE_APP_KEYCLOAK_REALM=${VUE_APP_KEYCLOAK_REALM}" >> .env.production.local
+RUN echo "VUE_APP_KEYCLOAK_CLIENT_ID=${VUE_APP_KEYCLOAK_CLIENT_ID}" >> .env.production.local
 
 RUN yarn build
 
